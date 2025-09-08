49081c000b48400bc9ade15532676eaff6f3e024
0xGeN02 <manuelmateodgl02@gmail.com>
2025-09-08 19:58:54 +0200

    start terminal cli

---
+++ cli/src/main.rs
@@ -0,0 +1,12 @@
+mod commands;
+
+fn main() {
+    println!("\n##################################################");
+    println!("###                                            ###");
+    println!("###          POST-QUANTUM COMPUTING            ###");
+    println!("###                                            ###");
+    println!("###          BLOCKCHAIN  ENCRYPTION            ###");
+    println!("###                                            ###");
+    println!("##################################################");
+    print!("\n");
+}
